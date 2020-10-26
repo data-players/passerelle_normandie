1c588f71b0b77aa40da62408c76849237715e783
simon louvet <simon.louvet.zen@gmail.com>
2020-10-27 00:45:57 +0100

    prod

--- server/services/api.service.js
+++ server/services/api.service.js
@@ -20,6 +20,10 @@ module.exports = {
   ],
   async started() {
     const sparqlRoutes = await this.broker.call('sparqlEndpoint.getApiRoutes');
+    let path=sparqlRoutes[0].path;
+    if (path.indexOf('/')===0){
+      path= path.substring(1);
+    }
     console.log(process.env.SEMAPPS_HOME_URL);
     console.log(new URL(process.env.SEMAPPS_HOME_URL).pathname);
     sparqlRoutes[0].path = new URL(process.env.SEMAPPS_HOME_URL).pathname + sparqlRoutes[0].path;

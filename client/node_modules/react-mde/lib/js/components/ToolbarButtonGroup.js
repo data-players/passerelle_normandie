24d405bfb08352aae25ed85fe34b3d2b5be54fbe
simon louvet <simon.louvet.zen@gmail.com>
2020-10-26 18:14:06 +0100

    init

--- client/node_modules/react-mde/lib/js/components/ToolbarButtonGroup.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ClassNames_1 = require("../util/ClassNames");
-exports.ToolbarButtonGroup = function (props) {
-    return (React.createElement("ul", { className: ClassNames_1.classNames("mde-header-group", { hidden: props.hidden }) }, props.children));
-};
--- client/node_modules/react-mde/lib/js/types/CommandIntruction.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INSERT_TEXT_AT_CURSOR = "INSERT_TEXT_AT_CURSOR";
-exports.SELECT_RANGE = "SELECT_RANGE";

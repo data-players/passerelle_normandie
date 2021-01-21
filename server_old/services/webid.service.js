067f08ae6bc2da70dae12385cbdfeb6c542d64e3
simon louvet <simon.louvet.zen@gmail.com>
2021-01-21 02:54:05 +0100

    semapps upgrade + inference

--- server_old/services/webid.service.js
+++
@@ -1,8 +0,0 @@
-const { WebIdService } = require('@semapps/webid');
-
-module.exports = {
-  mixins: [WebIdService],
-  settings: {
-    usersContainer: process.env.SEMAPPS_HOME_URL + 'persons'
-  }
-};

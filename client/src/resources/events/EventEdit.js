6d336815e69afe98320605c24b57e56ad614d766
BastienSig <bastien.siguier@gmail.com>
2021-05-05 16:28:37 +0200

    issue30 add seb date component

--- client/src/resources/events/EventEdit.js
+++ client/src/resources/events/EventEdit.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { AutocompleteArrayInput, SimpleForm, TextInput, DateTimeInput, CheckboxGroupInput } from 'react-admin';
+import { AutocompleteArrayInput, SimpleForm, TextInput, CheckboxGroupInput } from 'react-admin';
 import MarkdownInput from 'ra-input-markdown';
+import frLocale from 'date-fns/locale/fr';
 import { Edit } from '@semapps/archipelago-layout';
+import { DateTimeInput } from '@semapps/date-components';
 import { UriArrayInput } from '@semapps/semantic-data-provider';
 import EventTitle from './EventTitle';
 
@@ -13,8 +15,28 @@ const EventEdit = props => (
       <MarkdownInput multiline source="pair:description" label="Description" fullWidth />
       <TextInput source="pair:homePage" label="Site web" fullWidth />
       <TextInput source="pair:aboutPage" label="Réseaux sociaux" fullWidth />
-      <DateTimeInput source="pair:startDate" label="Date de début" fullWidth />
-      <DateTimeInput source="pair:endDate" label="Date de fin" fullWidth />
+      <DateTimeInput
+          source="pair:startDate"
+          options={{
+            format: 'dd/MM/yyyy à HH:mm',
+            ampm: false
+          }}
+          providerOptions={{
+            locale: frLocale
+          }}
+          fullWidth
+        />
+        <DateTimeInput
+          source="pair:endDate"
+          options={{
+            format: 'dd/MM/yyyy à HH:mm',
+            ampm: false
+          }}
+          providerOptions={{
+            locale: frLocale
+          }}
+          fullWidth
+        />
       <UriArrayInput label="Proposé par" reference="Organization" source="pair:deliveredBy">
         <AutocompleteArrayInput shouldRenderSuggestions={value => value.length > 1} optionText="pair:label" fullWidth />
       </UriArrayInput>

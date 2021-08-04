61c0a2ab0038a46d3c8aba387d51d44761e3ad3a
Simon Louvet <simon.louvet.zen@gmail.com>
2021-08-04 18:58:11 +0200

    filter from sector

--- client/src/resources/sectors/SectorShow.js
+++ client/src/resources/sectors/SectorShow.js
@@ -17,14 +17,16 @@ const mainImage = makeStyles({
   }
 });
 
-const MoreButton = ({record, source, type, more }) => {
-  var allItem = typeof(record[source]) === "string" ? [record[source]] : record[source]
-  if (Array.isArray(allItem)) {
-    if ( allItem.filter(p=>p.includes(type)).length > 5) {
-      return <Button component={Link} to={more}>More...</Button>
+const MoreButton = ({record, source, type, more, path, triggerLength }) => {
+  if(Array.isArray(record[source]) && record[source].filter(p=>p.includes(type)).length > triggerLength){
+    let to={
+      pathname: `../../${path}`,
+      search: `filter=${JSON.stringify({ 'pair:hasSector': record.id })}`,
     }
+    return <Button component={Link} to={to}>More...</Button>
+  } else {
+    return <> </>
   }
-  return <> </>
 }
 
 const SectorShow = props => {
@@ -33,49 +35,44 @@ const SectorShow = props => {
   return <Show title={<ProjectTitle />} {...props}>
     <ColumnShowLayout>
       <Column xs={12} sm={12} showLabel>
-      <ImageField source="pair:banner" classes={mainImageStyles}/>
-          <MarkdownField label="Description" source="pair:description" addLabel />
-          <ReferenceArrayField perPage={5} label="Projets" reference="Project" source="pair:sectorOf" filter={{type: "pair:Project"}} addLabel >
-            <SingleFieldList linkType="show" >
-              <AvatarField label={record => `${record['pair:label']}`} image="image" classes={{
-                parent: {
-                  width: '100px',
-                  margin : '10px'
-                }
-              }}/>       
-            </SingleFieldList>
-          </ReferenceArrayField>
-          <MoreButton source="pair:sectorOf" type="project" more={{
-                      pathname: '../../Project'
-                  }}/>
-        
-          <ReferenceArrayField label="Organisations" perPage={5} reference="Organization" source="pair:sectorOf" filter={{type: "pair:Organization"}} addLabel>
-            <SingleFieldList linkType="show" >
-                <AvatarField label={record => `${record['pair:label']}`} image="image" classes={{
-                parent: {
-                  width: '100px',
-                  margin : '10px'
-                }
-              }}/>   
-            </SingleFieldList>
-          </ReferenceArrayField>
-          <MoreButton source="pair:sectorOf" type="organizations" more={{
-                      pathname: '../../Organization'
-                  }}/>
+        <ImageField source="pair:banner" classes={mainImageStyles}/>
+        <MarkdownField label="Description" source="pair:description" addLabel />
+        <ReferenceArrayField perPage={5} label="Projets" reference="Project" source="pair:sectorOf" filter={{type: "pair:Project"}} addLabel >
+          <SingleFieldList linkType="show" >
+            <AvatarField label={record => `${record['pair:label']}`} image="image" classes={{
+              parent: {
+                width: '100px',
+                margin : '10px'
+              }
+            }}/>
+          </SingleFieldList>
+        </ReferenceArrayField>
+        <MoreButton source="pair:sectorOf" type="project" path="Project" triggerLength={5}/>
 
-          <ReferenceArrayField perPage={5} label="Evenement"  reference="Event" source="pair:sectorOf" filter={{type: "pair:Event"}} addLabel>
-            <SingleFieldList linkType="show" >
-                <AvatarField label={record => `${record['pair:label']}`} image="image" classes={{
-                parent: {
-                  width: '100px',
-                  margin : '10px'
-                }
-              }}/>   
-            </SingleFieldList>
-          </ReferenceArrayField>
-          <MoreButton source="pair:sectorOf" type="events"more={{
-                      pathname: '../../Event'
-                  }}/>
+        <ReferenceArrayField label="Organisations" perPage={5} reference="Organization" source="pair:sectorOf" filter={{type: "pair:Organization"}} addLabel>
+          <SingleFieldList linkType="show" >
+              <AvatarField label={record => `${record['pair:label']}`} image="image" classes={{
+              parent: {
+                width: '100px',
+                margin : '10px'
+              }
+            }}/>
+          </SingleFieldList>
+        </ReferenceArrayField>
+
+        <MoreButton source="pair:sectorOf" type="organizations" path="Organization" triggerLength={5}/>
+
+        <ReferenceArrayField perPage={5} label="Evenement"  reference="Event" source="pair:sectorOf" filter={{type: "pair:Event"}} addLabel>
+          <SingleFieldList linkType="show" >
+              <AvatarField label={record => `${record['pair:label']}`} image="image" classes={{
+              parent: {
+                width: '100px',
+                margin : '10px'
+              }
+            }}/>
+          </SingleFieldList>
+        </ReferenceArrayField>
+        <MoreButton source="pair:sectorOf" type="events" path="Event" triggerLength={5}/>
 
       </Column>
     </ColumnShowLayout>

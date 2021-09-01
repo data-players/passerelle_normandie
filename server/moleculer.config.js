70524b852a0550b6ab10b1f1e0f1c04311116b35
BastienSig <bastien.siguier@gmail.com>
2021-09-01 12:01:22 +0200

    add cache ldp service

--- server/moleculer.config.js
+++ server/moleculer.config.js
@@ -1,4 +1,22 @@
+const { CacherMiddleware } = require('@semapps/webacl');
+//const CacherMiddleware= require('./cacher')
+
+console.log("LOG ENV : ", process.env.SEMAPPS_REDIS_URL)
+
+// Use the cacher only if Redis is configured
+const cacherConfig =  {
+  type: 'Redis',
+  options: {
+    prefix: 'action',
+    ttl: 2592000, // Keep in cache for one month
+    redis: process.env.SEMAPPS_REDIS_URL
+  }
+};
+
 module.exports = {
   // You can set all ServiceBroker configurations here
   // See https://moleculer.services/docs/0.14/configuration.html
+  middlewares: [
+    CacherMiddleware(cacherConfig),
+  ]
 };

db2a2a0534e2bcba40d7a90d1314dcd15d2f9d03
BastienSig <bastien.siguier@gmail.com>
2021-09-07 11:07:26 +0200

    make a .env and fix cache

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { ListBase, useShowController, ShowContextProvider } from 'react-admin';
+import { useShowController, ShowContextProvider } from 'react-admin';
 import { makeStyles } from '@material-ui/core/styles';
-import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import { MapList } from '@semapps/geo-components';
-import { SimpleList as RaSimpleList } from 'react-admin';
-import ProjectFilterSidebar from './HomePagefilterSideBar';
 import PageShow from '../resources/pages/PageShow';
-import Typography from '@material-ui/core/Typography';
 
 const mainStyle = makeStyles(theme => ({
     h3 :{
@@ -26,7 +20,6 @@ const config = {
 const HomePage = () => {
 
   const style = mainStyle();
-
   return(
     <Grid container spacing={0} classes={style}>
       <Grid item md={12} sm={12} xs={12} >
--- server/moleculer.config.js
+++ server/moleculer.config.js
@@ -1,18 +1,3 @@
-const { CacherMiddleware } = require('@semapps/webacl');
-//const CacherMiddleware= require('./cacher')
-
-console.log("LOG ENV : ", process.env.SEMAPPS_REDIS_URL)
-
-// // Use the cacher only if Redis is configured
-// const cacherConfig =  {
-//   type: 'Redis',
-//   options: {
-//     prefix: 'action',
-//     ttl: 2592000, // Keep in cache for one month
-//     redis: process.env.SEMAPPS_REDIS_URL
-//   }
-// };
-
 const cacher = process.env.SEMAPPS_REDIS_URL
   ? {
     type: 'Redis',
@@ -26,7 +11,5 @@ const cacher = process.env.SEMAPPS_REDIS_URL
 
 
 module.exports = {
-  // You can set all ServiceBroker configurations here
-  // See https://moleculer.services/docs/0.14/configuration.html
   cacher
 };
